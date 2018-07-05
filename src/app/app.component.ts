f32a412bdc4abcb1624026611b1cef3c0e5c8216
Douglas Hiles <dhiles2@outlook.com>
2018-07-05 08:03:59 -0600

    1.0.4 name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { Observable, Subject, fromEvent, of } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'the ngx-gridboard 1.0.3 demo app';
+  title = 'the ngx-gridboard 1.0.4 demo app';
   activeItem: any;
   laneChanges: Subject<LaneChange> = new Subject();
 
